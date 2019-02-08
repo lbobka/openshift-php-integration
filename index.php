74f1b72bc420f6f33652ef5b2189389ac39e6972
Lars <lars.bobka@gmail.com>
2019-02-08 15:52:32 +0100

    inserted header css

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
-<?php
-echo 'hello openshift world';
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>CSS Website Layout</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  margin: 0;
+}
+
+/* Style the header */
+.header {
+  background-color: #8c141e;
+  padding: 20px;
+  text-align: center;
+  color: #FFF;
+}
+</style>
+</head>
+<body>
+
+<div class="header">
+  <h1>
+  <?php
+    echo 'Hello Openshift World';
+  ?>
+  </h1>
+</div>
+
+</body>
+</html>
